06d3e74d17df8469071320e9b322ce9f943e26ae
Dave Hinton <dave@4420.uk>
2023-12-03 09:55:43 +0000

    add noddy express server

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+console.log('Starting server...');
+
+const app = express();
+const port = parseInt(process.env.PORT ?? '3000', 10);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
