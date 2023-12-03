5e91e270ff74c4dcff445276afd5116df5ea7a43
Dave Hinton <dave@4420.uk>
2023-12-03 10:35:47 +0000

    shutdown express gracefully

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+["SIGTERM", "SIGINT"].forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`${signal} received: closing server`);
+    server.close(() => {
+      console.log("server closed");
+    });
+  });
 });
