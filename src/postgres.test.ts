e3236ca4d12c6edb1272a2678a456392148ac358
Dave Hinton <dave@4420.uk>
2023-12-07 10:04:37 +0000

    get db query to mostly work

--- src/postgres.test.ts
+++ src/postgres.test.ts
@@ -23,4 +23,61 @@ describe("getHistoricVehicle()", () => {
     // then
     await expect(promise).resolves.toBeNull();
   });
+
+  it("returns the state the vehicle was in at the given time", async () => {
+    // given
+    const vehicleId = 3;
+    const when = Instant.parse("2022-09-12T09:00:00Z");
+
+    const expectedState = "selling";
+
+    // when
+    const promise = db.getHistoricVehicle({ vehicleId, when });
+
+    // then
+    await expect(promise).resolves.toEqual(
+      expect.objectContaining({
+        state: expectedState,
+      }),
+    );
+  });
+
+  it("returns the make, model and id of the vehicle", async () => {
+    // given
+    const vehicleId = 3;
+    const when = Instant.parse("2022-09-12T09:00:00Z");
+
+    const expectedMake = "VW";
+    const expectedModel = "GOLF";
+
+    // when
+    const promise = db.getHistoricVehicle({ vehicleId, when });
+
+    // then
+    await expect(promise).resolves.toEqual(
+      expect.objectContaining({
+        vehicleId,
+        make: expectedMake,
+        model: expectedModel,
+      }),
+    );
+  });
+
+  it("returns a null state if we query for a time before the vehicle’s first event", async () => {
+    // given
+    const vehicleId = 3;
+    const when = Instant.parse("2022-09-01T09:00:00Z");
+
+    const expectedState = null;
+
+    // when
+    const promise = db.getHistoricVehicle({ vehicleId, when });
+
+    // then
+    await expect(promise).resolves.toEqual(
+      expect.objectContaining({
+        state: expectedState,
+      }),
+    );
+  });
 });
--- src/postgres.ts
+++ src/postgres.ts
@@ -31,7 +31,38 @@ class Postgres implements Database {
     readonly model: string;
     readonly state: string | null;
   }> {
-    return null;
+    const { rows } = await this.pool.query({
+      text: `
+            WITH
+                params (vehicle_id, when_in_history) AS (
+                    VALUES (
+                        $1::integer,
+                        TO_TIMESTAMP($2)
+                    )
+                ),
+                events (vehicle_id, state) AS (
+                    SELECT "vehicleId", state
+                    FROM "stateLogs", params
+                    WHERE timestamp <= params.when_in_history
+                    ORDER BY timestamp DESC
+                )
+            SELECT v.make, v.model, e.state
+            FROM vehicles v RIGHT JOIN events e ON v.id = e.vehicle_id, params
+            WHERE v.id = params.vehicle_id
+            LIMIT 1
+        `,
+      values: [params.vehicleId, params.when.toEpochMilli() / 1000],
+    });
+    if (rows.length === 0) {
+      return null;
+    }
+    const row = rows[0];
+    return {
+      vehicleId: params.vehicleId,
+      make: row.make,
+      model: row.model,
+      state: row.state,
+    };
   }
 }
 
